079f80accbab3a3544b483b12e0cabfd9863ee1c
salmeleh <salmeleh@gmail.com>
2016-05-24 19:52:39 -0500

    Initial Commit

---
+++ litterBoxTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  litterBoxTracker
+//
+//  Created by Stu Almeleh on 5/24/16.
+//  Copyright © 2016 Stu Almeleh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
