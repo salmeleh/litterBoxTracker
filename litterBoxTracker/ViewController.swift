1336485968e138a1f03fb173be3b026e630e05d4
salmeleh <salmeleh@gmail.com>
2016-07-08 13:00:31 -0500

    done for 7/8

--- litterBoxTracker/ViewController.swift
+++ litterBoxTracker/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        //self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
         objects = fetchAllEvents()
         
